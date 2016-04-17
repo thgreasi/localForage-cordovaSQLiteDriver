860783ecc9ecd135705f59ea30ae2cfa5e398fa8
Thodoris Greasidis <thgreasi@gmail.com>
2016-04-17 10:26:20 +0300

    feat: use webSqlDriver methods to reduce the implementation

--- src/localforage-cordovasqlitedriver.js
+++ src/localforage-cordovasqlitedriver.js
@@ -7,19 +7,11 @@
  * Copyright (c) 2015 Mozilla
  * Licensed under Apache 2.0 license.
  *
- * ======================================
- *
- * base64-arraybuffer
- * https://github.com/niklasvh/base64-arraybuffer
- *
- * Copyright (c) 2012 Niklas von Hertzen
- * Licensed under the MIT license.
  */
 (function() {
     'use strict';
 
     var globalObject = this;
-    var serializer = null;
 
     // // If cordova is not present, we can stop now.
     // if (!globalObject.cordova) {
@@ -72,6 +64,32 @@
         });
     });
 
+    function getSerializerPromise(localForageInstance) {
+        if (getSerializerPromise.result) {
+            return getSerializerPromise.result;
+        }
+        if (!localForageInstance || typeof localForageInstance.getSerializer !== 'function') {
+            Promise.reject(new Error(
+                'localforage.getSerializer() was not available! ' +
+                'localforage-cordovasqlitedriver required localforage v1.4+'));
+        }
+        getSerializerPromise.result = localForageInstance.getSerializer();
+        return getSerializerPromise.result;
+    }
+
+    function getWebSqlDriverPromise(localForageInstance) {
+        if (getWebSqlDriverPromise.result) {
+            return getWebSqlDriverPromise.result;
+        }
+        if (!localForageInstance || typeof localForageInstance.getDriver !== 'function') {
+            Promise.reject(new Error(
+                'localforage.getDriver() was not available! ' +
+                'localforage-cordovasqlitedriver requires localforage v1.4+'));
+        }
+        getWebSqlDriverPromise.result = localForageInstance.getDriver(localForageInstance.WEBSQL);
+        return getWebSqlDriverPromise.result;
+    }
+
     // Open the cordova sqlite plugin database (automatically creates one if one didn't
     // previously exist), using any options set in the config.
     function _initStorage(options) {
@@ -86,15 +104,6 @@
                             options[i].toString() : options[i];
             }
         }
-
-        var serializerPromise = new Promise(function(resolve, reject) {
-            if (typeof self.getSerializer !== 'function') {
-                throw new Error(
-                    'localforage.getSerializer() was not available! ' +
-                    'localforage-cordovasqlitedriver required localforage v1.3+');
-            }
-            self.getSerializer().then(resolve, reject);
-        });
 
         var dbInfoPromise = openDatabasePromise.then(function(openDatabase){
             return new Promise(function(resolve, reject) {
@@ -127,302 +136,17 @@
             });
         });
 
-        return serializerPromise.then(function(lib) {
-            serializer = lib;
+        var serializerPromise = getSerializerPromise(self);
+        var webSqlDriverPromise = getWebSqlDriverPromise(self);
+
+        return Promise.all([
+            serializerPromise,
+            webSqlDriverPromise,
+            dbInfoPromise
+        ]).then(function(results) {
+            dbInfo.serializer = results[0];
             return dbInfoPromise;
         });
-    }
-
-    function getItem(key, callback) {
-        var self = this;
-
-        // Cast the key to a string, as that's all we can set as a key.
-        if (typeof key !== 'string') {
-            window.console.warn(key +
-                                ' used as a key, but it is not a string.');
-            key = String(key);
-        }
-
-        var promise = new Promise(function(resolve, reject) {
-            self.ready().then(function() {
-                var dbInfo = self._dbInfo;
-                dbInfo.db.transaction(function(t) {
-                    t.executeSql('SELECT * FROM ' + dbInfo.storeName +
-                                 ' WHERE key = ? LIMIT 1', [key],
-                                 function(t, results) {
-                        var result = results.rows.length ?
-                                     results.rows.item(0).value : null;
-
-                        // Check to see if this is serialized content we need to
-                        // unpack.
-                        if (result) {
-                            result = serializer.deserialize(result);
-                        }
-
-                        resolve(result);
-                    }, function(t, error) {
-
-                        reject(error);
-                    });
-                });
-            }).catch(reject);
-        });
-
-        executeCallback(promise, callback);
-        return promise;
-    }
-
-    function iterate(iterator, callback) {
-        var self = this;
-
-        var promise = new Promise(function(resolve, reject) {
-            self.ready().then(function() {
-                var dbInfo = self._dbInfo;
-
-                dbInfo.db.transaction(function(t) {
-                    t.executeSql('SELECT * FROM ' + dbInfo.storeName, [],
-                        function(t, results) {
-                            var rows = results.rows;
-                            var length = rows.length;
-
-                            for (var i = 0; i < length; i++) {
-                                var item = rows.item(i);
-                                var result = item.value;
-
-                                // Check to see if this is serialized content
-                                // we need to unpack.
-                                if (result) {
-                                    result = serializer.deserialize(result);
-                                }
-
-                                result = iterator(result, item.key, i + 1);
-
-                                // void(0) prevents problems with redefinition
-                                // of `undefined`.
-                                if (result !== void(0)) {
-                                    resolve(result);
-                                    return;
-                                }
-                            }
-
-                            resolve();
-                        }, function(t, error) {
-                            reject(error);
-                        });
-                });
-            }).catch(reject);
-        });
-
-        executeCallback(promise, callback);
-        return promise;
-    }
-
-    function setItem(key, value, callback) {
-        var self = this;
-
-        // Cast the key to a string, as that's all we can set as a key.
-        if (typeof key !== 'string') {
-            window.console.warn(key +
-                                ' used as a key, but it is not a string.');
-            key = String(key);
-        }
-
-        var promise = new Promise(function(resolve, reject) {
-            self.ready().then(function() {
-                // The localStorage API doesn't return undefined values in an
-                // "expected" way, so undefined is always cast to null in all
-                // drivers. See: https://github.com/mozilla/localForage/pull/42
-                if (value === undefined) {
-                    value = null;
-                }
-
-                // Save the original value to pass to the callback.
-                var originalValue = value;
-
-                serializer.serialize(value, function(value, error) {
-                    if (error) {
-                        reject(error);
-                    } else {
-                        var dbInfo = self._dbInfo;
-                        dbInfo.db.transaction(function(t) {
-                            t.executeSql('INSERT OR REPLACE INTO ' +
-                                         dbInfo.storeName +
-                                         ' (key, value) VALUES (?, ?)',
-                                         [key, value], function() {
-                                resolve(originalValue);
-                            }, function(t, error) {
-                                reject(error);
-                            });
-                        }, function(sqlError) {
-                            // The transaction failed; check
-                            // to see if it's a quota error.
-                            if (sqlError.code === sqlError.QUOTA_ERR) {
-                                // We reject the callback outright for now, but
-                                // it's worth trying to re-run the transaction.
-                                // Even if the user accepts the prompt to use
-                                // more storage on Safari, this error will
-                                // be called.
-                                //
-                                // TODO: Try to re-run the transaction.
-                                reject(sqlError);
-                            }
-                        });
-                    }
-                });
-            }).catch(reject);
-        });
-
-        executeCallback(promise, callback);
-        return promise;
-    }
-
-    function removeItem(key, callback) {
-        var self = this;
-
-        // Cast the key to a string, as that's all we can set as a key.
-        if (typeof key !== 'string') {
-            window.console.warn(key +
-                                ' used as a key, but it is not a string.');
-            key = String(key);
-        }
-
-        var promise = new Promise(function(resolve, reject) {
-            self.ready().then(function() {
-                var dbInfo = self._dbInfo;
-                dbInfo.db.transaction(function(t) {
-                    t.executeSql('DELETE FROM ' + dbInfo.storeName +
-                                 ' WHERE key = ?', [key],
-                                 function() {
-                        resolve();
-                    }, function(t, error) {
-
-                        reject(error);
-                    });
-                });
-            }).catch(reject);
-        });
-
-        executeCallback(promise, callback);
-        return promise;
-    }
-
-    // Deletes every item in the table.
-    // TODO: Find out if this resets the AUTO_INCREMENT number.
-    function clear(callback) {
-        var self = this;
-
-        var promise = new Promise(function(resolve, reject) {
-            self.ready().then(function() {
-                var dbInfo = self._dbInfo;
-                dbInfo.db.transaction(function(t) {
-                    t.executeSql('DELETE FROM ' + dbInfo.storeName, [],
-                                 function() {
-                        resolve();
-                    }, function(t, error) {
-                        reject(error);
-                    });
-                });
-            }).catch(reject);
-        });
-
-        executeCallback(promise, callback);
-        return promise;
-    }
-
-    // Does a simple `COUNT(key)` to get the number of items stored in
-    // localForage.
-    function length(callback) {
-        var self = this;
-
-        var promise = new Promise(function(resolve, reject) {
-            self.ready().then(function() {
-                var dbInfo = self._dbInfo;
-                dbInfo.db.transaction(function(t) {
-                    // Ahhh, SQL makes this one soooooo easy.
-                    t.executeSql('SELECT COUNT(key) as c FROM ' +
-                                 dbInfo.storeName, [], function(t, results) {
-                        var result = results.rows.item(0).c;
-
-                        resolve(result);
-                    }, function(t, error) {
-
-                        reject(error);
-                    });
-                });
-            }).catch(reject);
-        });
-
-        executeCallback(promise, callback);
-        return promise;
-    }
-
-    // Return the key located at key index X; essentially gets the key from a
-    // `WHERE id = ?`. This is the most efficient way I can think to implement
-    // this rarely-used (in my experience) part of the API, but it can seem
-    // inconsistent, because we do `INSERT OR REPLACE INTO` on `setItem()`, so
-    // the ID of each key will change every time it's updated. Perhaps a stored
-    // procedure for the `setItem()` SQL would solve this problem?
-    // TODO: Don't change ID on `setItem()`.
-    function key(n, callback) {
-        var self = this;
-
-        var promise = new Promise(function(resolve, reject) {
-            self.ready().then(function() {
-                var dbInfo = self._dbInfo;
-                dbInfo.db.transaction(function(t) {
-                    t.executeSql('SELECT key FROM ' + dbInfo.storeName +
-                                 ' WHERE id = ? LIMIT 1', [n + 1],
-                                 function(t, results) {
-                        var result = results.rows.length ?
-                                     results.rows.item(0).key : null;
-                        resolve(result);
-                    }, function(t, error) {
-                        reject(error);
-                    });
-                });
-            }).catch(reject);
-        });
-
-        executeCallback(promise, callback);
-        return promise;
-    }
-
-    function keys(callback) {
-        var self = this;
-
-        var promise = new Promise(function(resolve, reject) {
-            self.ready().then(function() {
-                var dbInfo = self._dbInfo;
-                dbInfo.db.transaction(function(t) {
-                    t.executeSql('SELECT key FROM ' + dbInfo.storeName, [],
-                                 function(t, results) {
-                        var keys = [];
-
-                        for (var i = 0; i < results.rows.length; i++) {
-                            keys.push(results.rows.item(i).key);
-                        }
-
-                        resolve(keys);
-                    }, function(t, error) {
-
-                        reject(error);
-                    });
-                });
-            }).catch(reject);
-        });
-
-        executeCallback(promise, callback);
-        return promise;
-    }
-
-    function executeCallback(promise, callback) {
-        if (callback) {
-            promise.then(function(result) {
-                callback(null, result);
-            }, function(error) {
-                callback(error);
-            });
-        }
     }
 
     var cordovaSQLiteDriver = {
@@ -432,16 +156,38 @@
             return openDatabasePromise.then(function(openDatabase) {
                 return !!openDatabase;
             }).catch(function(){ return false; });
-        },
-        iterate: iterate,
-        getItem: getItem,
-        setItem: setItem,
-        removeItem: removeItem,
-        clear: clear,
-        length: length,
-        key: key,
-        keys: keys
+        }
     };
+
+    function wireUpDriverMethods(driver) {
+        var LibraryMethods = [
+            'clear',
+            'getItem',
+            'iterate',
+            'key',
+            'keys',
+            'length',
+            'removeItem',
+            'setItem'
+        ];
+
+        function wireUpDriverMethod(driver, methodName) {
+            driver[methodName] = function () {
+                var localForageInstance = this;
+                var args = arguments;
+                return getWebSqlDriverPromise(localForageInstance).then(function (webSqlDriver) {
+                    return webSqlDriver[methodName].apply(localForageInstance, args);
+                });
+            };
+        }
+
+        for (var i = 0, len = LibraryMethods.length; i < len; i++) {
+            var methodName = LibraryMethods[i];
+            wireUpDriverMethod(driver, LibraryMethods[i]);
+        }
+    }
+
+    wireUpDriverMethods(cordovaSQLiteDriver);
 
     if (moduleType === ModuleType.DEFINE) {
         define('cordovaSQLiteDriver', function() {
